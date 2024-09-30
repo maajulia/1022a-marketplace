262fbf1dbc48a6cffa68a5623342c79e675be3cf
maajulia <118356114+maajulia@users.noreply.github.com>
2024-09-30 10:07:01 -0400

    adaptação para servidor web

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+
+
+import express from 'express'
+import mysql from 'mysql2/promise'
+const app = express()
+app.get("/produtos", async(req,res)=>{
+    //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
+    //1 - Criar a conexão com o banco
+    try{
+        const conection = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser? process.env.db: "root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022a",
+            port:process.env.dbport? parseInt(process.env.dbport):3306
+        })
+        //2 - Realizar uma consulta na tabela
+        const [result, fields] = await conection.query("SELECT * from produtos")
+        //3 - Devolver os dados pra quem pediu
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Server ERROR")
+    }
+})
+app.listen(8000,()=>{
+    console.log("Iniciei o servidor")
+})

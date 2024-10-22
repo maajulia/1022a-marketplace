c8398ba534e8f7062b4cd272d96060523907681f
maajulia <118356114+maajulia@users.noreply.github.com>
2024-10-22 10:41:43 -0400

    arrumando e atualizando

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,43 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+import cors from 'cors';
 
-import express from 'express'
-import mysql from 'mysql2/promise'
-import cors from 'cors'
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.get("/produtos", async(req,res)=>{
-    //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
-    //1 - Criar a conexão com o banco
-    try{
-        const conection = await mysql.createConnection({
-            host: process.env.dbhost?process.env.dbhost:"localhost",
-            user:process.env.dbuser? process.env.dbuser: "root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
-            database:process.env.dbname?process.env.dbname:"banco1022a",
-            port:process.env.dbport? parseInt(process.env.dbport):3306
-        })
-        //2 - Realizar uma consulta na tabela
-        const [result, fields] = await conection.query("SELECT * from produtos")
-        await conection.end()
-        //3 - Devolver os dados pra quem pediu
-        res.send(result)
-    }catch(e){
-        res.status(500).send("Server ERROR")
-    }
-})
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get("/usuarios", async (req, res) => {
-    try {
-        const conection = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022a",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        });
-        
-        const [result, fields] = await conection.query("SELECT * FROM usuarios");
-        await conection.end();
-        
-        res.send(result);
-    } catch (e) {
-        res.status(500).send("Server ERROR");
-    }
+async function createConnection() {
+  return await mysql.createConnection({
+    host: process.env.dbhost ? process.env.dbhost : "localhost",
+    user: process.env.dbuser ? process.env.dbuser : "root",
+    password: process.env.dbpassword ? process.env.dbpassword : "",
+    database: process.env.dbname ? process.env.dbname : "banco1022a",
+    port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+  });
+}
+
+app.get("/produtos", async (req, res) => {
+  try {
+    const conection = await createConnection();
+    const [result] = await conection.query("SELECT * from produtos");
+    await conection.end();
+    res.send(result);
+  } catch (e) {
+    res.status(500).send("Server ERROR");
+  }
 });
 
+app.get("/usuarios", async (req, res) => {
+  try {
+    const conection = await createConnection();
+    const [result] = await conection.query("SELECT * from usuarios");
+    await conection.end();
+    res.send(result);
+  } catch (e) {
+    res.status(500).send("Server ERROR");
+  }
+});
 
-
-app.listen(8000,()=>{
-    console.log("Iniciei o servidor")
-})
+app.listen(8000, () => {
+  console.log("Iniciei o servidor");
+});
